c5f0b0c03d1540edb3b0a746b5639d347c2e6611
wangwuyuan <helloworldwwy@gmail.com>
2019-09-29 18:20:24 +0800

    no message

--- app/src/main/java/com/wwy/wanandroid/api/HttpRepository.kt
+++ app/src/main/java/com/wwy/wanandroid/api/HttpRepository.kt
@@ -1,9 +1,42 @@
 package com.wwy.wanandroid.api
 
+import android.annotation.SuppressLint
+import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import timber.log.Timber
+import java.util.*
+import java.util.concurrent.TimeUnit
+
 /**
  *@创建者wwy
  *@创建时间 2019/9/29 15:22
  *@描述
  */
-object HttpRepository1 {
+object HttpRepository {
+    var READ_TIME_OUT: Long = 5000
+    var CONNECT_TIME_OUT: Long = 5000
+    lateinit var mOkHttpClient: OkHttpClient.Builder
+    private fun HttpRepository1(hostType: Int) {
+        mOkHttpClient.readTimeout(READ_TIME_OUT, TimeUnit.MILLISECONDS)
+            .connectTimeout(CONNECT_TIME_OUT, TimeUnit.MILLISECONDS)
+            .addInterceptor(mLoggingInterceptor)
+
+    }
+
+    @SuppressLint("BinaryOperationInTimber")
+    private val mLoggingInterceptor = Interceptor { chain ->
+        val request = chain.request()
+        val t1 = System.nanoTime()
+        val response = chain.proceed(chain.request())
+        val t2 = System.nanoTime()
+        val contentType = response.body()?.contentType()
+        val content = response.body()?.string()
+        Timber.tag("wwy").d( "request url:" + request.url() + "\ntime:" + (t2 - t1) / 1e6 + "\nbody:" + content + "\n")
+        response.newBuilder()
+            .body(okhttp3.ResponseBody.create(contentType, content))
+            .build()
+
+
+    }
 }
