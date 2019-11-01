5ff0231bc9b5eef41086dad7068e0b71648067eb
wangwuyuan <helloworldwwy@gmail.com>
2019-11-01 17:59:01 +0800

    no message

--- app/src/main/java/com/wwy/wanandroid/bean/Article.kt
+++ app/src/main/java/com/wwy/wanandroid/bean/Article.kt
@@ -1,5 +1,8 @@
 package com.wwy.wanandroid.bean
 
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import java.io.Serializable
 
 /**
@@ -7,26 +10,29 @@ import java.io.Serializable
  *@创建时间 2019/10/8 11:26
  *@描述
  */
-data class Article( val id: Int,
-                    val originId: Int,
-                    val title: String,
-                    val chapterId: Int,
-                    val chapterName: String,
-                    val envelopePic: String,
-                    val link: String,
-                    val author: String,
-                    var shareUser: String,
-                    val origin: String,
-                    val publishTime: Long,
-                    val zan: Int,
-                    val desc: String,
-                    val visible: Int,
-                    val niceDate: String,
-                    val courseId: Int,
-                    var collect: Boolean,
-                    val apkLink:String,
-                    val projectLink:String,
-                    val superChapterId:Int,
-                    val superChapterName:String?,
-                    val type:Int,
-                    val fresh:Boolean):Serializable
+data class Article(
+    val key: Long,
+    val id: Int,
+    val author: String,
+    var shareUser: String,
+    val title: String,
+    val chapterName: String,
+    val superChapterName: String?,
+    val niceDate: String,
+    val originId: Int,
+    val chapterId: Int,
+    val envelopePic: String,
+    val link: String,
+    val origin: String,
+    val publishTime: Long,
+    val zan: Int,
+    val desc: String,
+    val visible: Int,
+    val courseId: Int,
+    var collect: Boolean,
+    val apkLink: String,
+    val projectLink: String,
+    val superChapterId: Int,
+    val type: Int,
+    val fresh: Boolean
+) : Serializable
