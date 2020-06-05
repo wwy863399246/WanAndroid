b6a0c1ee3d9c09718335c24d5f96b1016a234cb4
wangwuyuan <helloworldwwy@gmail.com>
2020-06-05 15:09:56 +0800

    添加公众号数据

---
+++ app/src/main/java/com/leshu/superbrain/ui/homeplaza/WeChatNumTypeFragment.kt
@@ -0,0 +1,102 @@
+package com.leshu.superbrain.ui.homepage
+
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.listener.OnLoadMoreListener
+import com.leshu.superbrain.R
+import com.leshu.superbrain.adapter.HomePageAdapter
+import com.leshu.superbrain.ui.base.BaseVMFragment
+import com.leshu.superbrain.view.loadpage.BasePageStateView
+import com.leshu.superbrain.view.loadpage.LoadPageView
+import com.leshu.superbrain.view.loadpage.SimpleLoadPageView
+import com.leshu.superbrain.vm.HomeProjectViewModel
+import kotlinx.android.synthetic.main.fragment_recycleview.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+
+class ProjectTypeFragment : BaseVMFragment<HomeProjectViewModel>(), OnLoadMoreListener {
+    override fun initVM(): HomeProjectViewModel = getViewModel()
+    override fun setLayoutResId(): Int = R.layout.fragment_recycleview
+    private val cid by lazy { arguments?.getInt(CID) }// cid==0是最新项目 否项目分类
+    private val homePageAdapter = HomePageAdapter()
+    private val loadPageView : BasePageStateView = SimpleLoadPageView()
+    private lateinit var rootView: LoadPageView
+    private var i: Int = 0
+
+    companion object {
+        private const val CID = "projectCid"
+        fun newInstance(cid: Int): ProjectTypeFragment {
+            val fragment = ProjectTypeFragment()
+            val bundle = Bundle()
+            bundle.putInt(CID, cid)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    override fun initView() {
+        rootView = activity?.let { activity -> loadPageView.getRootView(activity) } as LoadPageView
+        rootView.apply {
+            failTextView().onClick { refresh() }
+            noNetTextView().onClick { refresh() }
+        }
+
+        ArticleRv.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = homePageAdapter
+        }
+        homePageAdapter.apply {
+            loadMoreModule.setOnLoadMoreListener(this@ProjectTypeFragment)
+            isAnimationFirstOnly = true
+            setAnimationWithDefault(BaseQuickAdapter.AnimationType.ScaleIn)
+        }
+        refreshLayout.setOnRefreshListener { refresh() }
+        refreshLayout.setEnableLoadMore(false)
+    }
+
+    override fun startObserve() {
+        mViewModel.apply {
+            mProjectListModel.observe(this@ProjectTypeFragment, Observer {
+                if (it.isRefresh) refreshLayout.finishRefresh(it.isRefreshSuccess)
+                if (it.showEnd) homePageAdapter.loadMoreModule.loadMoreEnd()
+                it.loadPageStatus?.value?.let { loadPageStatus ->
+                    loadPageView.convert(
+                        rootView,
+                        loadPageStatus = loadPageStatus
+                    )
+                    homePageAdapter.setEmptyView(rootView)
+                }
+                it.showSuccess?.let { list ->
+                    homePageAdapter.apply {
+                        loadMoreModule.isEnableLoadMore = false
+                        if (it.isRefresh) setList(list) else addData(list)
+                        loadMoreModule.isEnableLoadMore = true
+                        loadMoreModule.loadMoreComplete()
+                    }
+                }
+                it.showError?.let { errorMsg ->//加载失败处理
+                    homePageAdapter.loadMoreModule.loadMoreFail()
+                }
+            })
+        }
+    }
+
+    override fun initData() {
+        if (i != 0) homePageAdapter.setList(null) //viewpge缓存4个界面 界面重新加载的时候清空数据重新获取
+        refresh()
+        i++
+    }
+
+    private fun refresh() {
+        homePageAdapter.loadMoreModule.isEnableLoadMore = false
+        cid?.let { mViewModel.loadProjectArticles(true, it) }
+    }
+
+    override fun onLoadMore() {
+        cid?.let { mViewModel.loadProjectArticles(false, it) }
+    }
+
+
+}
